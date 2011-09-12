5d65fa9f91e25c79487809d0f551957bd711b278
Chris Baglieri <chris.baglieri@gmail.com>
2011-09-11 22:22:08 -0400

    bottle first pass complete

---
+++ bottle/code/teachme_api/teachme_api.py
@@ -0,0 +1,60 @@
+from random import randint
+from mongokit import Connection, Document
+from bottle import get, post, put, delete, run
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+
+connection = Connection(MONGODB_HOST, MONGODB_PORT)
+
+@connection.register
+class Topic(Document):
+	__collection__ = 'topics'
+	__database__ = 'teachme'
+	structure = {
+		'_id': int,
+		'name': unicode,
+		'description': unicode,
+		'presenter': unicode
+	}
+	required_fields = ['name', 'description']
+	use_dot_notation = True
+
+@get('/topics')
+def show_topics():
+	topics = dict()
+	for topic in list(connection.Topic.find()):
+		topics[topic._id] = topic
+	return topics
+
+@get('/topics/:id')
+def show_topic(id):
+	topic = connection.Topic.find_one({'_id': int(id)})
+	return topic
+
+@post('/topics')
+def create_topic():
+	topic = connection.Topic()
+	topic._id = randint(1, 1000000)
+	topic.name = request.form['name']
+	topic.description = request.form['description']
+	topic.presenter = request.form['presenter']
+	topic.save()
+	return topic
+
+@put('/topics/:id')
+def update_topic(id):
+	topic = connection.Topic.find_one({'_id': int(id)})
+	topic.name = request.form['name']
+	topic.description = request.form['description']
+	topic.presenter = request.form['presenter']
+	topic.save()
+	return topic
+
+@delete('/topics/:id')
+def destroy_topic(id):
+	topic = connection.Topic.find_one({'_id': int(id)})
+	topic.delete()
+	return True
+
+run(host='localhost', port=8080, reloader=True)
